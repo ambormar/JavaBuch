fc7c91fcc7735d9ab03631b714a10c10668f4874
ambormar <marcoambord@gmx.ch>
2014-11-18 20:18:19 +0100

    change in gitignore, new Eclipse_Repo...

---
+++ src/uebungen04/Eclipse_Repository_FilesIgnorieren.java
@@ -0,0 +1,9 @@
+/* TODO .4.2.1.   eigene bemerkung !!!!!!!!!!!!!!!!!!!!
+ * 
+ * files die von prgramm beim compilieren erzeugt werden im repository nicht mit committen: => file > re-klick > team > ignore 
+ */
+package uebungen04;
+
+public class Eclipse_Repository_FilesIgnorieren {
+
+}
