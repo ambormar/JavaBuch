f7bd47184baae34a663119c2d112f1f7b5b45061
ambormar <marcoambord@gmx.ch>
2014-12-04 15:36:11 +0100

    create Eclipse_Progr....

---
+++ src/uebungen04/Eclipse_ProgrammKopieren_VonKompiZuKompi_ProgrammOeffnen.java
@@ -0,0 +1,21 @@
+/* TODO .4.2.1.   eigene bemerkung
+ * Eclipse_ProgrammKopieren_VonKompiZuKompi_ProgrammOeffnen
+ * 
+ * ECLIPSE PROGRAMM KOPIEREN VON KOMPI ZU KOMPI:
+ * 
+ * 		- Eclipse ist zwar ein programm, aber kein gängiges:
+ * 			=> so kann man z.b. Eclipse mit all seinen einstellungen & plugins etc. von einem komputer auf den anderen kopieren wie folgt:
+ * 
+ * 				-> Computer (C:) > Programm Files (Programme (x64)) > Eclipse (ganzen ordner mit allen inhalten) auf externe festplatte kopieren
+ * 				-> neuen Computer (C:) > Program Files (Programe (x64)) > Eclipse (ganzen ordner mit allen inhalten) reinkopieren
+ * 
+ * ECLIPSE IM NEUEN KOMPI ÖFFNEN:
+ * 
+ * 				-> Computer (C:) > Programm Files (Programme (x64)) > Eclipse > eclipse.exe doppelklicken
+ * 				-> bzw: eclipse.exe auf den desktop / untere symbolleiste kopieren
+ */
+package uebungen04;
+
+public class Eclipse_ProgrammKopieren_VonKompiZuKompi_ProgrammOeffnen {
+
+}
