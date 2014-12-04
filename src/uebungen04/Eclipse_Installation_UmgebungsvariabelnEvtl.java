ff97630225c267a62beb1889a79d0cf8d680471d
ambormar <marcoambord@gmx.ch>
2014-12-04 15:19:47 +0100

    comments Eclipse_Install...

--- src/uebungen04/Eclipse_Installation_UmgebungsvariabelnEvtl.java
+++ src/uebungen04/Eclipse_Installation_UmgebungsvariabelnEvtl.java
@@ -2,7 +2,7 @@ package uebungen04;
 
 /* TODO .4.1.1.   s.130
  * 
- * evtl. umgebungsvariabeln anpassen:	s.131
+ * evtl. umgebungsvariabeln anpassen:	s.131 / 135 / 138
  * 
  */
 public class Eclipse_Installation_UmgebungsvariabelnEvtl {
