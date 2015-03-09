24468d02ca26eb27a12f0e5c21877c0a980fc1c4
ambormar <marcoambord@gmx.ch>
2015-03-09 22:13:55 +0000

    create Color_..

---
+++ src/uebungen13/Color_Klasse_Konstruktoren_Methoden_FarbObjekte_RGB_HSB.java
@@ -0,0 +1,36 @@
+/* TODO 13.3.1.   s.420, ???
+ * class Color_Klasse_Konstruktoren_Methoden_FarbObjekte_RGB_HSB
+ * 
+ * DIE KLASSE COLOR:	
+ * 		
+ * 			=> IMPORT:		java.awt.Color
+ * 
+ * 			=> RGB-model:	-> Klasse definiert farbe über rot, grü, blau
+ * 							-> r,g & b -anteile je von 0 -255
+ * 
+ * 			=> HSB-model:	-> klasse definiert farbe über hue (grundton), saturation (sättigung), brightness (helligkeit) 	
+ * ???						-> h,s, & b -anteile 0 - 255 oder 0 - 100  ????????????????????????????????????????????????????????? (irgend noch abklären)
+ * 
+ * 			=> hat Methoden um RGB in HSB (und umgekehrt) umzurechenen
+ * 
+ * 			=> ALPHAWERTE:	-> für Transparenz, von 0 - 255, 0 = grösste transparenz
+ * 	
+ * 
+ *	13 FARB-OBJEKTE VON COLOR FÜR DIE GRUNDFARBEN:		
+ * 
+ * 			public final static Color white		, lightgray, gray, darkgray, black, red, pink, orange, yellow, green, magenta, cyan, blue 
+ * 
+ * 	
+ * 	WICHTIGSTE KONSTRUKTOREN VON COLOR: 
+ * 
+ * 			
+ * 
+ * 
+ * 
+ */
+
+package uebungen13;
+
+public class Color_Klasse_Konstruktoren_Methoden_FarbObjekte_RGB_HSB {
+
+}
